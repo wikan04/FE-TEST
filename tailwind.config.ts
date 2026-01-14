5a868389debbfc0a66c7414c9b997a9d0a1ca4d1
wikan04 <wikanworldstar@gmail.com>
2026-01-15 05:35:15 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#1e40af",
+          dark: "#1e3a8a",
+        },
       },
     },
   },
